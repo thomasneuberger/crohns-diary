7134a86bc4a6dcdd43f0b9a7fda01bad5972b6a3
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-06-25 02:39:09 +0200

    Add simple UI

---
+++ CrohnsDiary.App/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace CrohnsDiary.App.Pages;
+
+public partial class Home
+{
+    [Inject]
+    public required ISnackbar Snackbar { get; set; }
+    private DateTime? SelectedDate { get; set; } = DateTime.Now;
+
+    private TimeSpan? SelectedTime { get; set; } = DateTime.Now.TimeOfDay;
+
+    private void OnSave()
+    {
+        Snackbar.Add("Saved.", Severity.Success);
+    }
+}
