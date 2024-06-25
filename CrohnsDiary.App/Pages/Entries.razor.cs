9b603b6a91c97b43ac52b3efc84a7ad055eaad5a
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-06-25 11:15:26 +0200

    Add entries list

---
+++ CrohnsDiary.App/Pages/Entries.razor.cs
@@ -0,0 +1,44 @@
+﻿using CrohnsDiary.App.Database;
+using CrohnsDiary.App.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CrohnsDiary.App.Pages;
+
+public partial class Entries
+{
+    [Inject]
+    public required EntryDatabase Database { get; set; }
+
+    private DateTime? SelectedDate { get; set; }
+
+    private DateTime? EntriesListDate { get; set; }
+    private IReadOnlyList<Entry> EntriesOnSelectedDate { get; set; } = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        SelectedDate = DateTime.Now.Date;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        if (SelectedDate.HasValue && EntriesListDate != SelectedDate)
+        {
+            await UpdateEntries(SelectedDate.Value);
+        }
+    }
+
+    private async Task UpdateEntries(DateTime selectedDate)
+    {
+        EntriesOnSelectedDate = await Database.Entries
+            .Where(nameof(Entry.Timestamp))
+            .Between(selectedDate, selectedDate.AddDays(1))
+            .ToList();
+
+        Console.WriteLine($"Entries found: {EntriesOnSelectedDate.Count}");
+
+        EntriesListDate = selectedDate;
+
+        StateHasChanged();
+    }
+}
