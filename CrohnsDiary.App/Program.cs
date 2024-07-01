128f4aa701037956a6475b7d5a184020742a7fd7
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-07-01 15:44:36 +0200

    Add imprint template

--- CrohnsDiary.App/Program.cs
+++ CrohnsDiary.App/Program.cs
@@ -1,10 +1,10 @@
+using BlazorDexie.Extensions;
+using CrohnsDiary.App;
+using CrohnsDiary.App.Database;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using CrohnsDiary.App;
+using MudBlazor;
 using MudBlazor.Services;
-using BlazorDexie.JsModule;
-using CrohnsDiary.App.Database;
-using BlazorDexie.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +15,6 @@ builder.Services.AddScoped<EntryDatabase>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 
 await builder.Build().RunAsync();
