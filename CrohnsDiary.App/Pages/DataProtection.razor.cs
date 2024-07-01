fcda16ec8db9e774d382e2469dcf3cba6c81272e
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-07-01 17:15:32 +0200

    Add data protection declaration

---
+++ CrohnsDiary.App/Pages/DataProtection.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace CrohnsDiary.App.Pages;
+
+public partial class DataProtection
+{
+    [Inject]
+    public required HttpClient Http { get; set; }
+
+    public string Content { get; set; } = """
+                                          Loading...
+                                          """;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var language = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+        var response = await Http.GetAsync($"/Dataprotection.{language}.md");
+        if (response.IsSuccessStatusCode)
+        {
+            Content = await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            Content = await Http.GetStringAsync("/Dataprotection.md");
+        }
+    }
+}
