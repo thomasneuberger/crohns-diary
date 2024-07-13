ccae5965135c6192431bcef2baf179f33a25c00a
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-07-13 21:00:57 +0200

    Add about page

---
+++ CrohnsDiary.App/Pages/About.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace CrohnsDiary.App.Pages;
+
+public partial class About
+{
+    [Inject]
+    public required HttpClient Http { get; set; }
+
+    public string Content { get; set; } = """
+                                          Loading...
+                                          """;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var language = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+        var response = await Http.GetAsync($"/About.{language}.md");
+        if (response.IsSuccessStatusCode)
+        {
+            Content = await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            Content = await Http.GetStringAsync("/About.md");
+        }
+    }
+}
