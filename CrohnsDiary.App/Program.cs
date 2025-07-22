43cbe7483a09d05f0e873dacbd6e49fa343d17b6
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2025-07-22 16:12:14 +0200

    Update packages

--- CrohnsDiary.App/Program.cs
+++ CrohnsDiary.App/Program.cs
@@ -11,7 +11,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddDexieWrapper();
+builder.Services.AddBlazorDexie();
 builder.Services.AddScoped<EntryDatabase>();
 builder.Services.AddScoped<ISettingsDatabase, SettingsDatabase>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
