5cf1902d7aecb2f4d407575ed63284239e9ac417
Thomas Neuberger <23504477+thomasneuberger@users.noreply.github.com>
2025-07-22 16:13:01 +0200

    Update packages (#17)

--- CrohnsDiary.App/Database/EntryDatabase.cs
+++ CrohnsDiary.App/Database/EntryDatabase.cs
@@ -1,12 +1,11 @@
 ﻿using BlazorDexie.Database;
-using BlazorDexie.JsModule;
+using BlazorDexie.Options;
 using CrohnsDiary.App.Models;
 
 namespace CrohnsDiary.App.Database;
 
-public class EntryDatabase(
-    IModuleFactory jsModuleFactory)
-    : Db("EntryDatabase", 1, Array.Empty<DbVersion>(), jsModuleFactory)
+public class EntryDatabase(BlazorDexieOptions blazorDexieOptions)
+    : Db<EntryDatabase>("EntryDatabase", 1, [], blazorDexieOptions)
 {
     public Store<Entry, Guid> Entries { get; set; } = new(nameof(Entry.Id), nameof(Entry.Timestamp));
 }
